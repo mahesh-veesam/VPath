0d3e37b716e47ed19fb53e93cfb34defb238134d
mahesh-veesam <veesammaheshmahee@gmail.com>
2024-09-01 01:59:57 +0530

    main page almost completedwith responsiveness

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
+let btn1 = document.querySelector(".box1")
+let pBtn1 = document.querySelectorAll(".p1")
+let btn2 = document.querySelector(".box2")
+let pBtn2 = document.querySelectorAll(".p2")
 
+btn1.addEventListener("mouseover",function(){
+    for(btn of pBtn1){
+        btn.style.color = "rgb(212, 198, 212)";
+        btn.style.fontWeight = "700"
+    }
+})
+btn1.addEventListener("mouseout",function(){
+    for(btn of pBtn1){
+        btn.style.color = "rgb(142, 147, 151)";
+        btn.style.fontWeight = "600"
+    }
+})
+btn2.addEventListener("mouseover",function(){
+    for(btn of pBtn2){
+        btn.style.color = "rgb(212, 198, 212)";
+        btn.style.fontWeight = "700"
+    }
+})
+btn2.addEventListener("mouseout",function(){
+    for(btn of pBtn2){
+        btn.style.color = "rgb(142, 147, 151)";
+        btn.style.fontWeight = "600"
+    }
+})
 
 
 
